19b1769f83dbee37259ca497807b024d905a5c8f
hwhaocool <hwhaocool@gmail.com>
2021-12-18 19:38:37 +0800

    update

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# compile phase
+cd compile
 echo "unpack lombok"
 mvn dependeny:unpack
 
+cd ..
 echo "reanme *.SCL.lombok to *.class"
 sh ./src/main/resources/scripts/rename-class.sh
 
-echo "assembly a jar for compile"
+echo "assembly a temp jar for compile"
 mvn jar:jar
 
 echo "generate class"
